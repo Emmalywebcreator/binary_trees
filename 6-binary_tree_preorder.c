552c4487aac3f33290c483d4c3e5e019db90062e
Ohwoka Emmanuel <emmyprime2015@gmail.com>
2024-06-05 10:56:40 +0100

    binary tree preorder updated

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -13,7 +13,7 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
     if (tree == NULL || func == NULL)
         return;
 
-    func(tree->n);  // Call the function with the value of the node
-    binary_tree_preorder(tree->left, func);  // Traverse the left subtree
-    binary_tree_preorder(tree->right, func);  // Traverse the right subtree
+    func(tree->n);
+    binary_tree_preorder(tree->left);
+    binary_tree_preorder(tree->right);
 }
