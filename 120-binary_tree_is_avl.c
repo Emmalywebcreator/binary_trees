79455eee2bf687580bb4fe54c56a011bedf095fa
Babaoni147 <obahedwin@ymail.com>
2024-06-07 22:44:08 +0100

    Task-30 updated

--- 120-binary_tree_is_avl.c
+++ 120-binary_tree_is_avl.c
@@ -8,7 +8,7 @@ int binary_tree_is_avl(const binary_tree_t *tree);
 /**
  * height - Measures the height of a binary tree.
  * @tree: A pointer to the root node of the tree to measure the height.
- * Return: If tree is NULL return 0, else return height.
+ * Return: 0, else return height.
  */
 size_t height(const binary_tree_t *tree)
 {
@@ -32,7 +32,7 @@ size_t height(const binary_tree_t *tree)
  */
 int is_avl_helper(const binary_tree_t *tree, int lo, int hi)
 {
-	size_t lft, rit, diff;
+	size_t lhgt, rhgt, diff;
 
 	if (tree != NULL)
 	{
@@ -40,7 +40,7 @@ int is_avl_helper(const binary_tree_t *tree, int lo, int hi)
 			return (0);
 		lhgt = height(tree->left);
 		rhgt = height(tree->right);
-		diff = lft > rit ? lft - rit : rit - lft;
+		diff = lhgt > rhgt ? lhgt - rhgt : rhgt - lhgt;
 		if (diff > 1)
 			return (0);
 		return (is_avl_helper(tree->left, lo, tree->n - 1) &&
