5724a2d20f0baf131eab30b8343851482e0f34ee
Babaoni147 <obahedwin@ymail.com>
2024-06-07 14:59:51 +0100

    Updated

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -4,7 +4,7 @@
  * binary_tree_sibling - Finds the sibling of a node.
  * @node: A pointer to the node to find the sibling.
  *
- * Return: A pointer to the sibling node, or NULL if no sibling or node is NULL.
+ * Return: Node, or NULL if no sibling or node is NULL.
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
@@ -15,7 +15,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 
 	if (node->parent->left == node)
 	{
-		return node->parent->right;
+		return (node->parent->right);
 	}
 	else if (node->parent->right == node)
 	{
